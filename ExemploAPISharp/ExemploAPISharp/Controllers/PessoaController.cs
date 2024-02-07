df84798f9e972b905b4832db569e929ec3fced7e
JoseRobson2000 <129907542+JoseRobson2000@users.noreply.github.com>
2024-02-06 22:18:31 -0300

    ExemploAPI

---
+++ ExemploAPISharp/ExemploAPISharp/Controllers/PessoaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ExemploAPISharp.Model;
+
+namespace ExemploAPISharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet("GetPessoa")]
+        public Pessoa GetPessoa()
+        {
+            var vPessoa = new Pessoa();
+            vPessoa.Id = 1;
+            vPessoa.Nome = "José Robson";
+            vPessoa.NomeroCelular = "(18) 12345-6789";
+
+            return vPessoa;
+
+        }
+
+    }
+}
---
+++ ExemploAPISharp/ExemploAPISharp/Model/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace ExemploAPISharp.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NomeroCelular { get; set; }
+        public DateOnly DataNascimento { get; set; }
+    }
+}
